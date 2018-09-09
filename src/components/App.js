669c4ac5c6c755d004d10e499a8dcc1cc716a2cb
Sean Baines <mrseanbaines@gmail.com>
2018-09-09 17:40:46 +0100

    Add basic global styles and add title and slogan

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+`
+
+const Slogan = styled.h6`
+  opacity: 0.4;
+`
 
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <AppWrapper>
+        <h1>pomo</h1>
+        <Slogan>The handy pomodoro timer</Slogan>
+      </AppWrapper>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './index.css';
-import App from './components/App';
+import '~/src/reset.css';
+import '~/src/index.css';
+import App from '~/src/components/App';
 
 render(<App />, document.getElementById('root'));
